810d620a9a7ed770bca834fdb49ae66642cc65f6
beycaCC <he.yiyang@northeastern.edu>
2026-01-15 15:12:36 -0500

    applied pagination to the stock getAll() method

--- api/Helpers/QueryObject.cs
+++ api/Helpers/QueryObject.cs
@@ -11,5 +11,7 @@ namespace api.Helpers
         public String? CompanyName { get; set; } = null;
         public String? SortBy { get; set; } = null;
         public bool isDecending { get; set; } = false;
+        public int PageNumber { get; set; } = 1;
+        public int PageSize { get; set; } = 20;
     }
 }
